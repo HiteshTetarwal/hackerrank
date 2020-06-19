65422fb306b5ac83bb5170e7aac092bbaf4f1854
unknown <imhitesh007@gmail.com>
2020-06-19 11:48:44 +0530

    minimum abs difference between two numbers

---
+++ greedy_algorithms/minimum_absolute_difference.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr.sort()
+    minimal = 999999999999999
+    for i in range(len(arr)-1):
+        if abs(arr[i]-arr[i+1])<=minimal:
+           minimal = abs(arr[i]-arr[i+1])
+    return minimal
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
